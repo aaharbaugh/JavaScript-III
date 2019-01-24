43d69b7d9ae4aa8a2ba2fcf95e137fc0a9621ef0
aaharbaugh <aaronmharbaugh@gmail.com>
2019-01-23 20:52:35 -0600

    finished this.js and protoypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -204,9 +204,9 @@ Humanoid.prototype.greet = function () {
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
 
-  evilMan.attack(archer)
-  goodMan.heal(archer)
-  evilMan.attack(archer)
-  evilMan.attack(archer)
-  evilMan.attack(archer)
+  console.log(evilMan.attack(archer));
+  console.log(goodMan.heal(archer));
+  console.log(evilMan.attack(archer));
+  console.log(evilMan.attack(archer));
+  console.log(evilMan.attack(archer));
   //"Lilith was removed from the game"
--- assignments/this.js
+++ assignments/this.js
@@ -2,10 +2,12 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window Binding. By default. this is bound to the window which is the browser object. 
-    
-* 2. 
-* 3. 
-* 4. 
+
+* 2. Implicit binding is when we are within  object and we are referring to itself. A value within that object is referenced using dot notation. 
+
+* 3. a new binding is when you have an object and via parameters you define each part of the object and you refer to the values  as .this.something 
+
+* 4. explicit. you have already bound the object and are doing it again through another function outside of the object. 
 *
 * write out a code example of each explanation above
 */
@@ -44,4 +46,8 @@ const myFirstCar = new Vehicle('Nissan Altima', '2015')
 // Principle 4
 
 // code example for Explicit Binding
+
+const mySecondCar = new Vehicle('BMW', '2018')
+
+myFirstCar.description.apply(mySecondCar) //now refers to mysecondcar when going through MFC object. 
 
